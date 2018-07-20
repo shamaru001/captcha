8af47513c7da9a4932f8f4af9519d5da5f279500
Shamaru <Shamaru001@gmail.com>
2018-07-20 16:03:36 -0400

    [added] font and new functions

--- src/captcha.php
+++ src/captcha.php
@@ -1,4 +1,5 @@
-<?php namespace shamaru001\captcha;
+<?php 
+namespace shamaru001\captcha;
 
 /**
 *  This class is to instance the captcha
@@ -9,6 +10,36 @@
 *  @author Shamaru Primera
 */
 class captcha{
+    private $image = null;
+    private $width  = 400;
+    private $heigth = 200;
+    private $RGBBackgroundColor = [0,100,0];
+    private $RGBImageBackground = null;
+    private $RGBTextColor = [233,14,91];
+    private $textSize = 5;
+    private $text = "text on screen";
+
+    public function __construct($textSize = 5, $RandomTextSize = false, $text=""){
+
+        if ($RandomTextSize){
+            $this->textSize = rand(1, 10);
+        }
+        else{
+            $this->textSize = $textSize;
+        }
+
+        if (!empty($text)){
+            $this->text = $text;
+        }
+        else{
+            $textCreator = "";
+            while (strlen($textCreator) <= (int) $this->textSize){
+                $textCreator .= mt_rand();
+            }
+            $this->text = substr($textCreator, 0, $this->textSize);
+        }
+
+    }
 
     /**
     * Sample method 
@@ -20,13 +51,34 @@ class captcha{
     *
     * @return string
     */
-    private function createImage($width = 100, $heigth=40, array $RGBBackground = [0,0,0], array $RGBTextColor = [233,14,91] ){
-        $im = imagecreate(110, 20);
-        $background_color = imagecolorallocate($im, $RGBBackground[0], $RGBBackground[1], $RGBBackground[2]);
-        $text_color = imagecolorallocate($im, 233, 14, 91);
-        imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
-        return imagepng($im);
+    public function setRandomValues(){
+        $this->RGBTextColor = [rand(0,255),rand(0,255),rand(0,255)];
+        $this->RGBBackgroundColor = [rand(0,255),rand(0,255),rand(0,255)];
     }
-}
 
-$captcha = new shamaru001\captcha\captcha;
+    public function setTextColor($red, $green, $blue){
+        $this->RGBTextColor = [(int)$red, (int) $green, (int) $blue];
+    }
+
+    public function setBackgroundColor($red, $green, $blue){
+        $this->RGBBackgroundColor = [(int)$red, (int) $green, (int) $blue];
+    }
+
+    public function getText(){
+        return $this->text;
+    }
+
+    function showImage(){
+        $this->image = imagecreate($this->width, $this->heigth);
+        $background_color = imagecolorallocate($this->image, $this->RGBBackgroundColor[0], $this->RGBBackgroundColor[1], $this->RGBBackgroundColor[2]);
+        $text_color = imagecolorallocate($this->image, $this->RGBTextColor[0], $this->RGBTextColor[1], $this->RGBTextColor[2]);
+
+        $font_path =  realpath(__DIR__."/fonts/Pacifico.ttf");
+        //imagestring($this->image, 132, 100, 100,  $this->getText(), $text_color);
+        imagettftext($this->image, 50, 0, 0, 100, $text_color, $font_path, $this->getText());
+
+        imagepng($this->image);
+    }
+
+
+}
